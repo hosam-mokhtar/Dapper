65277f2aa73133f57f0d2c374eba2b71555409de
Hossam <hossam.mokhtar.fcis@gmail.com>
2025-12-28 16:57:42 +0200

    add Main method
    - test ProductManager CRUD operations
    - include code to run BenchmarkDapperVsEfcore with BenchmarkRunner

---
+++ Dapper/DapperVsEfCore/Program.cs
@@ -0,0 +1,18 @@
+﻿using BenchmarkDotNet.Running;
+using DapperVsEfCore.DapperManager;
+
+namespace DapperVsEfCore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ProductManager manager = new ProductManager();
+
+            //var result = manager.GetById(1);
+            //Console.WriteLine(result?.ProductName);
+
+            //var summary = BenchmarkRunner.Run<BenchmarkDapperVsEfcore>();
+        }
+    }
+}
