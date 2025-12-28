161508d6fa5c30e576ac12221b97cf094602d02e
Hossam <hossam.mokhtar.fcis@gmail.com>
2025-12-28 16:38:09 +0200

    add Category, Product, and Supplier entities

---
+++ Dapper/DapperVsEfCore/Entities/Category.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DapperVsEfCore.Entities
+{
+    [Index(nameof(CategoryName), Name = "CategoryName")]
+    public class Category
+    {
+        [Key]
+        [Column("CategoryID")]
+        public int CategoryId { get; set; }
+
+        [Required]
+        [StringLength(15)]
+        public string CategoryName { get; set; }
+
+        [Column(TypeName = "ntext")]
+        public string Description { get; set; }
+
+        [Column(TypeName = "image")]
+        public byte[] Picture { get; set; }
+
+
+        [InverseProperty(nameof(Product.Category))]
+        public virtual ICollection<Product> Products { get; set; } = [];
+    }
+}
