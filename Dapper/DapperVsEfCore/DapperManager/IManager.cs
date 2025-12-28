22a6cac5cad49b3b76bc202a920941dcff38c911
Hossam <hossam.mokhtar.fcis@gmail.com>
2025-12-28 16:43:10 +0200

    add IManager interface
    add generic IManager interface with basic CRUD operations

---
+++ Dapper/DapperVsEfCore/DapperManager/IManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperVsEfCore.DapperManager
+{
+    public interface IManager<T>
+    {
+        List<T> GetAll();
+        T? GetById(long id);
+        bool Add(T item);
+        bool Update(T item);
+        bool Delete(long id);
+    }
+}
