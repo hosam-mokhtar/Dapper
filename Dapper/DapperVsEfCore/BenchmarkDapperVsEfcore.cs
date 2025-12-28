a8b0201d2cb2b4b4a0ad277b25d2e8b5debf72ff
Hossam <hossam.mokhtar.fcis@gmail.com>
2025-12-28 16:52:13 +0200

    add BenchmarkDapperVsEfcore class for Dapper vs EF Core
    - create BenchmarkDapperVsEfcore class
    - add DapperExample method using stored procedure
    - add EFCoreExample method loading DbSet<Product>
    - use [Benchmark] attribute for performance measurement

---
+++ Dapper/DapperVsEfCore/BenchmarkDapperVsEfcore.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using Dapper;
+using DapperVsEfCore.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace DapperVsEfCore
+{
+    public class BenchmarkDapperVsEfcore
+    {
+        NorthWindDbContext dbContext = new NorthWindDbContext();
+        DbConnection dbconnection;
+
+        public BenchmarkDapperVsEfcore()
+        {
+            dbconnection = dbContext.Database.GetDbConnection();
+        }
+
+        [Benchmark]
+        public void DapperExample()
+        {
+            var products = dbconnection.Query("SelectAllProducts", commandType: System.Data.CommandType.StoredProcedure)
+                                       .ToList();
+            Console.WriteLine(products.Count);
+        }
+
+        [Benchmark]
+        public void EFCoreExample()
+        {
+            dbContext.Products.Load();
+
+            Console.WriteLine(dbContext.Products.Local.Count);
+        }
+    }
+}
